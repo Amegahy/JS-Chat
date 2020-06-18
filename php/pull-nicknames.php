2bc103049dd084e11a6fe293227f52d0332777dd
Amegahy <alex.megahy@hotmail.co.uk>
2020-06-18 11:11:06 +0100

    pull nickname

--- php/pull-nicknames.php
+++ php/pull-nicknames.php
@@ -10,6 +10,7 @@
 *   Include the database connection
 */
 include 'db-con.php';
+include 'blocked-users.php';
 
 /*
 *   Pull nicknames
@@ -17,24 +18,27 @@ include 'db-con.php';
 $chatID = $_SESSION["chat_id"];
 $users = $_SESSION["chat_users"];
 $response = []; // Response array
+$blocked = blockedSearch($conn);
 
 foreach ($users as &$user) {
-    $username = $user; // User name
-    $userNN = ""; // User nickname
-    $userNNSql = "SELECT * from chat_users WHERE chat_name = '". $chatID ."' AND user = '". $user ."'";
-    $userNNResult = $conn->query($userNNSql);
-
-    if (!$conn -> query($userNNSql)) { // Test for errors
-        echo("Error: " . $conn -> error);
-        exit;
-    } else {
-        if ($userNNResult->num_rows > 0) { // User has a nickname
-            while($row = $userNNResult->fetch_assoc()) { 
-                $userNN = $row['nickname'];
+    if(empty(in_array($user, $blocked))){ // User is not blocked
+        $username = $user; // User name
+        $userNN = ""; // User nickname
+        $userNNSql = "SELECT * from chat_users WHERE chat_name = '". $chatID ."' AND user = '". $user ."'";
+        $userNNResult = $conn->query($userNNSql);
+    
+        if (!$conn -> query($userNNSql)) { // Test for errors
+            echo("Error: " . $conn -> error);
+            exit;
+        } else {
+            if ($userNNResult->num_rows > 0) { // User has a nickname
+                while($row = $userNNResult->fetch_assoc()) { 
+                    $userNN = $row['nickname'];
+                }
             }
         }
+        $response[] = array('name'=> $username, 'nn'=> $userNN);
     }
-    $response[] = array('name'=> $username, 'nn'=> $userNN);
 }
 $json = json_encode($response);
 
